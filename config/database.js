75cd6e1335287d4ff1308c443dd99b1654fa6ece
J o s e p h  S a m i r <68662601+JosephSamirL@users.noreply.github.com>
2022-03-20 04:17:41 +0200

    fixed missing params

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV'),
-        port: env.int('DATABASE_PORT'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
